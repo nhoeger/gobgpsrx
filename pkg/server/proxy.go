76fbff32521466d9b7b5ac65d28dc7154217cfc5
nhoeger <nils.hoeger@unibw.de>
2023-08-07 12:19:56 +0200

    Fixed ASCones Integration

--- pkg/server/proxy.go
+++ pkg/server/proxy.go
@@ -19,36 +19,51 @@ const (
 )
 
 type VerifyMessage struct {
-	PDU                   string
-	Flags                 string
-	OriginResultSoruce    string
-	PathResultSoruce      string
-	ASPAResultSoruce      string
-	reserved              string
-	ASPathType            string
-	ASRelationType        string
-	Length                string
-	origin_default_result string
-	path_default_result   string
-	aspa_default_result   string
-	prefix_len            string
-	request_token         string
-	prefix                string
-	origin_AS             string
-	length_path_val_data  string
-	num_of_hops           string
-	bgpsec_length         string
-	afi                   string
-	safi                  string
-	prefix_len_bgpsec     string
-	ip_pre_add_byte_a     string
-	ip_pre_add_byte_b     string
-	ip_pre_add_byte_c     string
-	ip_pre_add_byte_d     string
-	local_as              string
-	as_path_list          string
-	path_attribute        string
-	bgpsec                string
+	PDU                  string
+	Flags                string
+	OriginResultSource   string
+	PathResultSource     string
+	ASPAResultSource     string
+	ASConesResultSource  string
+	reserved             string
+	ASPathType           string
+	ASRelationType       string
+	Length               string
+	OriginDefaultResult  string
+	PathDefaultResult    string
+	ASPADefaultResult    string
+	ASConesDefaultResult string
+	prefix_len           string
+	request_token        string
+	prefix               string
+	origin_AS            string
+	length_path_val_data string
+	num_of_hops          string
+	bgpsec_length        string
+	afi                  string
+	safi                 string
+	prefix_len_bgpsec    string
+	ip_pre_add_byte_a    string
+	ip_pre_add_byte_b    string
+	ip_pre_add_byte_c    string
+	ip_pre_add_byte_d    string
+	local_as             string
+	as_path_list         string
+	path_attribute       string
+	bgpsec               string
+}
+
+type VerifyNotify struct {
+	PDU              string
+	ResultType       string
+	OriginResult     string
+	PathResult       string
+	ASPAResult       string
+	ASConesResult    string
+	Zero             string
+	Length           string
+	RequestToken     string
+	UpdateIdentifier string
 }
 
 type BGPsecDate struct {
--- pkg/server/rpkimanager.go
+++ pkg/server/rpkimanager.go
@@ -2,8 +2,6 @@ package server
 
 import "C"
 import (
-	"bytes"
-	"encoding/binary"
 	"encoding/hex"
 	"fmt"
 	"net"
@@ -11,7 +9,6 @@ import (
 	"strings"
 	"sync"
 	"time"
-	"unsafe"
 
 	//_ "github.com/osrg/gobgp/table"
 	_ "os"
@@ -39,64 +36,67 @@ type rpkiManager struct {
 // Callback function: The proxy can call this function when the SRx-Server sends a verify notify
 // Input is a raw string containing the message from the server and a pointer to the rpkimanager
 func handleVerifyNotify(input string, rm rpkiManager) {
-	log.Debug(input)
+	log.Debug("Input: ", input)
+	vn := VerifyNotify{
+		PDU:              input[:2],
+		ResultType:       input[2:4],
+		OriginResult:     input[4:6],
+		PathResult:       input[6:8],
+		ASPAResult:       input[8:10],
+		ASConesResult:    input[10:12],
+		Zero:             input[12:16],
+		Length:           input[16:24],
+		RequestToken:     input[24:32],
+		UpdateIdentifier: input[32:40],
+	}
 
-	// result type (ASPA, AS-Cones, ..) from the SRx-Server
-	result_type := input[2:4]
-
-	// ID assigned by the SRx-Server
-	update_identifer := input[len(input)-8:]
-
-	// Token assigned by the RPKI manager
-	request_token := input[len(input)-16 : len(input)-8]
-
-	// Actual validation result
-	result := input[8:10]
-	log.Debug("+----------------------------------------+")
-	log.Debug("Update identifier: ", update_identifer)
-	log.Debug("Request Token:     ", request_token)
-	log.Debug("Cached Updates:    ", len(rm.Updates))
-	log.Debug("Result before:     ", result)
+	if log.GetLevel() == log.DebugLevel {
+		printValRes(vn)
+	}
 
 	// Iterating through all stores updates to find the matching one
 	for i, update := range rm.Updates {
-		log.Debug("ID:    ", update.local_id)
-		log.Debug("SRx-ID:", update.srx_id)
 		loc_ID := fmt.Sprintf("%08X", update.local_id)
 
 		// Found the correct update -> set SRxID
-		if strings.ToLower(loc_ID) == strings.ToLower(request_token) {
-			log.Debug("Path for Update:   ", update.fsmMsg.PathList)
-			log.Debug("In if Statement")
-			log.Debug("Changing Srx ID of update")
-			update.srx_id = update_identifer
-			log.Debug("srx ID:            ", update.srx_id)
-			log.Debug("+----------------------------------------+")
+		if strings.ToLower(loc_ID) == strings.ToLower(vn.RequestToken) {
+			update.srx_id = vn.UpdateIdentifier
 			return
 		}
 
 		// Found the matching token -> process result
-		if update.srx_id == update_identifer {
-			log.Debug("Result for update: ", result)
-			if result_type == "04" {
-				log.Debug("Received new information for aspa validation.")
-				num, err := strconv.ParseInt(result[1:], 10, 64)
-				log.Debug("Time needed: ", time.Since(update.time))
-				if err != nil {
-					fmt.Println("Conversion error:", err)
-					return
-				}
-
+		if update.srx_id == vn.UpdateIdentifier {
+			if vn.ResultType == "04" {
 				// Valid update
-				if result == "00" {
-					log.Debug("Adding Update")
-
+				if vn.ASPAResult == "00" {
 					// Process the BGP update
 					rm.Server.ProcessValidUpdate(update.peer, update.fsmMsg, update.bgpMsg)
 					rm.Updates = append(rm.Updates[:i], rm.Updates[i+1:]...)
-					update.aspa = int(num)
 					return
 				} else {
+					rm.Updates = append(rm.Updates[:i], rm.Updates[i+1:]...)
+					return
+				}
+			}
+			// ASCones Result
+			if vn.ResultType == "08" {
+				log.Debug("In If")
+				// Valid update
+				if vn.ASConesResult == "00" {
+
+					log.Debug("In If2")
+					// Process the BGP update
+					rm.Server.ProcessValidUpdate(update.peer, update.fsmMsg, update.bgpMsg)
+					rm.Updates = append(rm.Updates[:i], rm.Updates[i+1:]...)
+					return
+				} else {
+					// Update invalid
+					log.Debug("INVALID UPDATE")
+					log.Debug("INVALID UPDATE")
+					log.Debug("INVALID UPDATE")
+					log.Debug("INVALID UPDATE")
+					log.Debug("INVALID UPDATE")
+					log.Debug("INVALID UPDATE")
 					rm.Updates = append(rm.Updates[:i], rm.Updates[i+1:]...)
 					return
 				}
@@ -137,47 +137,43 @@ func (rm *rpkiManager) validate(peer *peer, m *bgp.BGPMessage, e *fsmMsg) {
 		}
 		// Create new message for each path
 		vm := VerifyMessage{
-			PDU:                   "03",
-			OriginResultSoruce:    "01",
-			PathResultSoruce:      "01",
-			ASPAResultSoruce:      "01",
-			reserved:              "00",
-			ASPathType:            "02",
-			ASRelationType:        "04",
-			Length:                "00000044",
-			origin_default_result: "03",
-			path_default_result:   "03",
-			aspa_default_result:   "03",
-			prefix_len:            "18",
-			request_token:         fmt.Sprintf("%08X", update.local_id),
-			prefix:                "00000000",
-			origin_AS:             "0000fdec",
-			length_path_val_data:  "00000008",
-			bgpsec_length:         "0000",
-			afi:                   "0000",
-			safi:                  "00",
-			prefix_len_bgpsec:     "00",
-			ip_pre_add_byte_a:     "00000000",
-			ip_pre_add_byte_b:     "00000000",
-			ip_pre_add_byte_c:     "00000000",
-			ip_pre_add_byte_d:     "00000000",
-			local_as:              fmt.Sprintf("%08X", rm.AS),
-			as_path_list:          "",
-			bgpsec:                "",
+			PDU:                  "03",
+			OriginResultSource:   "01",
+			PathResultSource:     "01",
+			ASPAResultSource:     "01",
+			reserved:             "01",
+			ASPathType:           "02",
+			ASRelationType:       "04",
+			Length:               "00000044",
+			OriginDefaultResult:  "03",
+			PathDefaultResult:    "03",
+			ASPADefaultResult:    "0303",
+			prefix_len:           "18",
+			request_token:        fmt.Sprintf("%08X", update.local_id),
+			prefix:               "00000000",
+			origin_AS:            "0000fdec",
+			length_path_val_data: "00000008",
+			bgpsec_length:        "0000",
+			afi:                  "0000",
+			safi:                 "00",
+			prefix_len_bgpsec:    "00",
+			ip_pre_add_byte_a:    "00000000",
+			ip_pre_add_byte_b:    "00000000",
+			ip_pre_add_byte_c:    "00000000",
+			ip_pre_add_byte_d:    "00000000",
+			local_as:             fmt.Sprintf("%08X", rm.AS),
+			as_path_list:         "",
+			bgpsec:               "",
 		}
 		log.Info("Time since Start:", time.Since(rm.StartTime))
 		if rm.Server.bgpConfig.Global.Config.ASPA {
 			log.Debug("ASPA")
 			vm.Flags = "84"
-			vm.reserved = "00"
-			vm.aspa_default_result = "03"
-			vm.ASPAResultSoruce = "01"
+			//vm.reserved = "00"
 		} else if rm.Server.bgpConfig.Global.Config.ASCONES {
 			log.Debug("ASCones")
 			vm.Flags = "88"
-			vm.reserved = ""
-			vm.aspa_default_result = "0303"
-			vm.ASPAResultSoruce = "0101"
+			//vm.reserved = ""
 		} else if peer.fsm.pConf.Config.BgpsecEnable {
 			vm.bgpsec = rm.GenerateBGPSecFields(e)
 		} else {
@@ -208,13 +204,13 @@ func (rm *rpkiManager) validate(peer *peer, m *bgp.BGPMessage, e *fsmMsg) {
 		vm.origin_AS = fmt.Sprintf("%08X", asList[len(asList)-1])
 		vm.num_of_hops = fmt.Sprintf("%04X", path.GetAsPathLen())
 		tmpInt := 4 * path.GetAsPathLen()
-		//vm.Length = fmt.Sprintf("%08X", 61+tmp_int)
-		vm.Length = fmt.Sprintf("%08X", 60+tmpInt)
+		vm.Length = fmt.Sprintf("%08X", 61+tmpInt)
+		//vm.Length = fmt.Sprintf("%08X", 60+tmpInt)
 		vm.length_path_val_data = fmt.Sprintf("%08X", tmpInt)
 		vm.origin_AS = fmt.Sprintf("%08X", path.GetSourceAs())
 
 		if log.GetLevel() == log.DebugLevel {
-			printValMessage(vm)
+			printValReq(vm)
 		}
 		updatesToSend = append(updatesToSend, structToString(vm))
 		rm.Updates = append(rm.Updates, &update)
@@ -230,7 +226,7 @@ func (rm *rpkiManager) validate(peer *peer, m *bgp.BGPMessage, e *fsmMsg) {
 }
 
 func (rm *rpkiManager) GenerateBGPSecFields(e *fsmMsg) string {
-	log.Debug("Generating BGPsec data.")
+	/*log.Debug("Generating BGPsec data.")
 	bgpSecString := ""
 
 	m := e.MsgData.(*bgp.BGPMessage)
@@ -351,26 +347,27 @@ func (rm *rpkiManager) GenerateBGPSecFields(e *fsmMsg) string {
 					Addr:   [16]byte{},
 				}*/
 
-				//pxip := prefixAddr
-				//copy(px.Addr[:], pxip)
-				//px.Pack(unsafe.Pointer(prefix2))
-				/* comment out for performance measurement
-				C.PrintSCA_Prefix(*prefix2)
-				*/
-				log.Debug("prefix2 : %#v", prefix2)
+	//pxip := prefixAddr
+	//copy(px.Addr[:], pxip)
+	//px.Pack(unsafe.Pointer(prefix2))
+	/* comment out for performance measurement
+	C.PrintSCA_Prefix(*prefix2)
+	*/ /*
+		log.Debug("prefix2 : %#v", prefix2)
 
-				valData.nlri = prefix2
-				log.Debug("valData : %#v", valData)
-				log.Debug("valData.bgpsec_path_attr : %#v", valData.bgpsec_path_attr)
-				/* comment out for performance measurement
-				C.printHex(C.int(bs_path_attr_length), valData.bgpsec_path_attr)
-				*/
-				log.Debug("valData.nlri : %#v", *valData.nlri)
+		valData.nlri = prefix2
+		log.Debug("valData : %#v", valData)
+		log.Debug("valData.bgpsec_path_attr : %#v", valData.bgpsec_path_attr)
+		/* comment out for performance measurement
+		C.printHex(C.int(bs_path_attr_length), valData.bgpsec_path_attr)
+	*/ /*
+					log.Debug("valData.nlri : %#v", *valData.nlri)
 
+				}
 			}
 		}
-	}
-	return bgpSecString
+		return bgpSecString*/
+	return ""
 }
 
 // NewRPKIManager Create new RPKI manager instance
@@ -421,20 +418,22 @@ func (rm *rpkiManager) SetAS(as uint32) error {
 	return nil
 }
 
-func printValMessage(vm VerifyMessage) {
+func printValReq(vm VerifyMessage) {
 	log.Debug("+----------------------------------+")
 	log.Debug("PDU:                   ", vm.PDU)
 	log.Debug("Flags:                 ", vm.Flags)
-	log.Debug("OriginResultSoruce:    ", vm.OriginResultSoruce)
-	log.Debug("PathResultSoruce:      ", vm.PathResultSoruce)
-	log.Debug("ASPAResultSoruce:      ", vm.ASPAResultSoruce)
+	log.Debug("OriginResultSoruce:    ", vm.OriginResultSource)
+	log.Debug("PathResultSoruce:      ", vm.PathResultSource)
+	log.Debug("ASPAResultSoruce:      ", vm.ASPAResultSource)
+	log.Debug("ASConesResultSoruce:   ", vm.ASConesResultSource)
 	log.Debug("reserved:              ", vm.reserved)
 	log.Debug("ASPathType:            ", vm.ASPathType)
 	log.Debug("ASRelationType:        ", vm.ASRelationType)
 	log.Debug("Length:                ", vm.Length)
-	log.Debug("origin_default_result: ", vm.origin_default_result)
-	log.Debug("path_default_result:   ", vm.path_default_result)
-	log.Debug("aspa_default_result:   ", vm.aspa_default_result)
+	log.Debug("OriginDefaultResult:   ", vm.OriginDefaultResult)
+	log.Debug("PathDefaultResult:     ", vm.PathDefaultResult)
+	log.Debug("ASPADefaultResult:     ", vm.ASPADefaultResult)
+	log.Debug("ASConesDefaultResult:  ", vm.ASConesDefaultResult)
 	log.Debug("prefix_len:            ", vm.prefix_len)
 	log.Debug("request_token:         ", vm.request_token)
 	log.Debug("prefix:                ", vm.prefix)
@@ -452,5 +451,20 @@ func printValMessage(vm VerifyMessage) {
 	log.Debug("local_as:              ", vm.local_as)
 	log.Debug("as_path_list:          ", vm.as_path_list)
 	log.Debug("path_attribute:        ", vm.path_attribute)
+	log.Debug("+----------------------------------+")
+}
+
+func printValRes(vn VerifyNotify) {
+	log.Debug("+----------------------------------+")
+	log.Debug("PDU:               ", vn.PDU)
+	log.Debug("ResultType:        ", vn.ResultType)
+	log.Debug("OriginResult:      ", vn.OriginResult)
+	log.Debug("PathResult:        ", vn.PathResult)
+	log.Debug("ASPAResult:        ", vn.ASPAResult)
+	log.Debug("ASConesResult:     ", vn.ASConesResult)
+	log.Debug("Zero:              ", vn.Zero)
+	log.Debug("Length:            ", vn.Length)
+	log.Debug("RequestToken:      ", vn.RequestToken)
+	log.Debug("UpdateIdentifier:  ", vn.UpdateIdentifier)
 	log.Debug("+----------------------------------+")
 }
